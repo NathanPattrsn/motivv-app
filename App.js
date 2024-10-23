1500803034e78173bae82608ab133dabea60ecdf
NathanPattrsn <79115357+NathanPattrsn@users.noreply.github.com>
2024-10-24 00:44:11 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -3,18 +3,27 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import MainScreen from './navigation/MainScreen';
 import OtherScreen from './navigation/DailyQuoteScreen'; // Example additional screen
+import LoginScreen from './navigation/LoginScreen';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+  const onLogin = () => {
+    // Handle what happens after login here
+    console.log("User logged in!");
+    // You can navigate to the main screen or update app state
+  };
+
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={MainScreen} />
         <Drawer.Screen name="Quote of the Day" component={OtherScreen} />
+        <Drawer.Screen 
+          name="Login" 
+          component={(props) => <LoginScreen {...props} onLogin={onLogin} />} // Pass the onLogin function correctly
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
-
-
---
+++ auth.js
@@ -0,0 +1,8 @@
+import Auth0 from 'react-native-auth0';
+
+const auth0 = new Auth0({
+  domain: 'dev-tzeirv7j1x38z22a.us.auth0.com',
+  clientId: 'oUVMbhbNS6WLBC7MKV8XdNzCSY7zAktB',
+});
+
+export default auth0;
